f7b623ee5a5d88afbd2a82baec39ad33174de2a3
OmarElFakih <omarelfakih97@gmail.com>
2019-06-01 15:30:37 -0400

    rotationAdded

---
+++ EC-GameJam4/Assets/_Scripts/EventManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour //SINGLETON (does not survive between scenes)
+{   
+    public static EventManager manager = null;
+
+    //event
+    public delegate void Timer();
+    public static Timer OnTimer;
+
+    [SerializeField]
+    private float timerIntervals = 0;
+
+    private float nextInterval = 0;
+
+    private void Awake()
+    {
+        if (manager == null)
+        {
+            manager = this;
+        }
+        else if (manager != null && manager != this)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+    private void Start()
+    {
+        nextInterval = Time.time + timerIntervals;
+    }
+
+    private void Update()
+    {
+        if (Time.time > nextInterval)
+        {
+            if (OnTimer != null)
+            {
+                OnTimer();
+            }
+            nextInterval = Time.time + timerIntervals;
+        }
+    }
+
+
+}
---
+++ EC-GameJam4/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour //SINGLETON
+{
+    //turns static reference to itself
+    public static GameManager manager = null;
+
+
+
+    //turns the first instance of this class into a singleton
+    private void Awake()
+    {
+        if (manager == null)
+        {
+            manager = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else if (manager != null && manager != this)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    
+
+
+}
