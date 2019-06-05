152bc6cf4f71b928557db91cb1630bdc2f1b6a80
OmarElFakih <omarelfakih97@gmail.com>
2019-06-05 09:21:53 -0400

    touch raycast created

---
+++ EC-GameJam4/Assets/_Scripts/Player/TouchRaycaster.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TouchRaycaster : MonoBehaviour
+{
+    [SerializeField]
+    private bool OnMobile;
+
+    public delegate void RaycastRoutine();
+    public RaycastRoutine raycastRoutine;
+
+    private void Start()
+    {
+        if (OnMobile)
+        {
+            raycastRoutine = TouchRaycast;
+        }
+        else
+        {
+            raycastRoutine = ClickRaycast;
+        }
+    }
+
+    private void Update()
+    {
+        raycastRoutine?.Invoke();
+    }
+
+
+    public void TouchRaycast()
+    {
+        if (Input.touchCount == 0)
+        {
+            return;
+        }
+
+        foreach (Touch t in Input.touches)
+        {
+            if (t.phase == TouchPhase.Began)
+            {
+                Vector3 _touchPos = Camera.main.ScreenToWorldPoint(t.position);
+                RaycastHit2D hit = Physics2D.Raycast(_touchPos, Vector2.zero);
+
+                if (hit)
+                {
+                    //hit routine
+                }
+
+            }
+        }
+    }
+
+    public void ClickRaycast()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 _mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(_mousePos, Vector3.zero);
+            if (hit)
+            {
+                Debug.Log(hit.transform.name);
+
+            }
+
+        }
+    }
+
+
+}
+
+
---
+++ EC-GameJam4/Assets/_Scripts/Player/TouchTarget.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TouchTarget : MonoBehaviour
+{
+    public UnityEvent OnTouched;
+
+    
+}
